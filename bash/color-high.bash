277c76e39913e027563af088a80c1ad3812c7de1
Ian Thompson <ian@quornian.co.uk>
2012-10-21 15:56:03 -0400

    Set link colour to blue - more consistent between low and high schemes

--- bash/color-high.bash
+++ bash/color-high.bash
@@ -3,7 +3,7 @@ export TERM="xterm-256color"
 
 # Other program colour options
 export GREP_COLORS="ms=01;34"
-export LS_COLORS="fi=38;5;7:di=38;5;3:ow=38;5;3:ln=38;5;8:ex=38;5;15:mi=38;5;9:su=07"
+export LS_COLORS="fi=38;5;7:di=38;5;3:ow=38;5;3:ln=38;5;4:ex=38;5;15:mi=38;5;9:su=07"
 
 # Colour options for the shell initialization
 if [[ ! -z "$SSH_CONNECTION" ]]
