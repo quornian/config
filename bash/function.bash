2c4060030ef12f21460404c82bc25a35bdb05bb8
Ian Thompson <ian@quornian.co.uk>
2013-03-31 14:40:13 -0400

    Bash: prompt fixes for jobs display, signal names on exit now portable
    Jobs now appear in the order they were started, which is a nice side-effect

--- bash/function.bash
+++ bash/function.bash
@@ -35,29 +35,7 @@ set-title() {
 
 # Return the name of a given signal
 signal-name() {
-    if [[ -z "$exitcode" ]]
-    then
-        echo "Usage: signal-name SIGNAL_NUMBER"
-    fi
-    case "$1" in
-         1) signame=SIGHUP;;   2) signame=SIGINT;;
-         3) signame=SIGQUIT;;  4) signame=SIGILL;;
-         5) signame=SIGTRAP;;  6) signame=SIGABRT;;
-         7) signame=SIGBUS;;   8) signame=SIGFPE;;
-         9) signame=SIGKILL;; 10) signame=SIGUSR1;;
-        11) signame=SIGSEGV;; 12) signame=SIGUSR2;;
-        13) signame=SIGPIPE;; 14) signame=SIGALRM;;
-        15) signame=SIGTERM;; 16) signame=SIGSTKFLT;;
-        17) signame=SIGCHLD;; 18) signame=SIGCONT;;
-        19) signame=SIGSTOP;; 20) signame=SIGTSTP;;
-        21) signame=SIGTTIN;; 22) signame=SIGTTOU;;
-        23) signame=SIGURG;;  24) signame=SIGXCPU;;
-        25) signame=SIGXFSZ;; 26) signame=SIGVTALRM;;
-        27) signame=SIGPROF;; 28) signame=SIGWINCH;;
-        29) signame=SIGIO;;   30) signame=SIGPWR;;
-        31) signame=SIGSYS;;   *) signame=UNKNOWN;;
-    esac
-    echo "$signame"
+    echo "SIG$(kill -l "$1")"
 }
 
 # Cached system values
--- bash/prompt.bash
+++ bash/prompt.bash
@@ -6,29 +6,25 @@
 # Git branch indicator
 function __prompt_git() {
     git rev-parse 2>/dev/null || return
-    b=$(git branch | sed -n 's/^\* \(.*\)/\1/p')
-    [ "$b" != "(no branch)" ] || b=$(git log -1 --pretty=%h)
-    echo "[$b]"
+    local bramch=$(git branch | sed -n 's/^\* \(.*\)/\1/p')
+    [ "$branch" != "(no branch)" ] || branch=$(git log -1 --pretty=%h)
+    echo " [$branch]"
 }
 
 # Background jobs indicator
 function __prompt_jobs() {
-    local pre='\[([0-9]+)\][-+]?\s+'
-    local post='\s+([A-Za-z0-9_/]+).*'
-    local names="$(jobs | sed -nr 's/'"${pre}${1}${post}"'/\2\&/p')"
-    echo "$names" | tr '\n' ' '
+    jobs | awk '/Stopped/ {printf " '"$(__cx 31)"'%s^Z", $3}
+                /Running/ {printf " '"$(__cx 32)"'%s&", $3}'
 }
 
 # Function to join the parts together
 function __make_prompt() {
-
     local ident="\[$(__cx "$__color_prompt_ident")\]\h:"
     local path="\[$(__cx "$__color_prompt_path")\]\w/"
     local git="\[$(__cx "$__color_prompt_git")\]\$(__prompt_git)"
-    local jobinfo="\[$(__cx 31)\]\$(__prompt_jobs Stopped)"
-    jobinfo="$jobinfo\[$(__cx 32)\]\$(__prompt_jobs Running)"
+    local jobinfo="\[$(__cx 31)\]\$(__prompt_jobs)"
     local prompt="\[$(__cx "$__color_prompt")\]\\$ \[$(__cx)\]"
-    export PS1="\n${ident} ${path} ${git} ${jobinfo}\n${prompt}"
+    export PS1="\n${ident} ${path}${git}${jobinfo}\n${prompt}"
 }
 
 __make_prompt
